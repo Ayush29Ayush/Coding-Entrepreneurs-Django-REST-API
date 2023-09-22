1cd5228c10d0618c9e337f2e7e61207b40444f87
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-22 13:48:10 +0530

    Created first API view and url

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,14 +2,16 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
+# endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
-print(get_response.text) # print raw text response
-print(get_response.status_code)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
 #! HTTP Request -> HTML
 #! REST API HTTP Request -> JSON
 #! JavaScript Object Nototion ~ Python Dict
-# print(get_response.json())
-# print(get_response.status_code)
+print(get_response.json())
+print(get_response.json()['message'])
+print(get_response.status_code)
