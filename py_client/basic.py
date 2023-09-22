2da126fc000c6fa5b1960f0a078c41598957a816
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-22 15:19:08 +0530

    Checking contents of a request and returning them as response

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
+get_response = requests.get(endpoint, params={"abc":123}, json={"query": "Hello world"}) # HTTP Request
 # print(get_response.text) # print raw text response
 # print(get_response.status_code)
 
@@ -13,5 +13,5 @@ get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Requ
 #! REST API HTTP Request -> JSON
 #! JavaScript Object Nototion ~ Python Dict
 print(get_response.json())
-print(get_response.json()['message'])
-print(get_response.status_code)
+# print(get_response.json()['message'])
+# print(get_response.status_code)
