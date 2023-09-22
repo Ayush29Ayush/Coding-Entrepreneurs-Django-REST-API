abca19ddb404deb2991a8808651186ae00eadf87
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-22 15:49:13 +0530

    Updated view to get Product model data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,28 +1,14 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
-
-# def api_home(request, *args, **kwargs):
-#     return JsonResponse({"message": "Hi there, this is your Django API response!!"})
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    print(request.GET) # url query params
-    print(request.POST)
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dict
-    except:
-        pass
-    print(data)
-    # print(data.keys())
-    # data['headers'] = request.headers # request.META ->
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
     return JsonResponse(data)
-
-{'query': 'Hello world', 'params': {'abc': ['123']}, 'headers': {'Content-Length': '24', 'Content-Type': 'application/json', 'Host': 'localhost:8000', 'User-Agent': 'python-requests/2.31.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}, 'content_type': 'application/json'}
