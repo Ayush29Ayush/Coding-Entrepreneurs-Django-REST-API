06c99c39297aebd42d2a9f201e70919c9c7793cf
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-23 12:19:42 +0530

    Checking out Response and api_view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,19 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().order_by("?").first() # This will give a queryset
+    print("model_data => ",model_data)
     data = {}
     if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=["id", "title", "price"]) # This will convert the queryset to a dictionary 
+        print("Data =>",data)
+    return Response(data) # This will return clean JSON type response
