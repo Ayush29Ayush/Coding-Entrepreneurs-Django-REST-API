c3df53b85d426014cb7d4788aa5512b1e8333f07
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-23 21:00:31 +0530

    Checking out generics API RetrieveAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+from .models import Product
+from .serializers import ProductSerializer
+
+#! RetrieveAPIView => Used for read-only endpoints to represent a single model instance. Provides a get method handler.
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk' ??
+
+
+product_detail_view = ProductDetailAPIView.as_view()
