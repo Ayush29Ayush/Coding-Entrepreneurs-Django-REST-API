9912b8e28331740d5d5790f63d7af252e8a61ddf
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-23 13:43:44 +0530

    Used serializer to get clean required JSON data

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,17 +3,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
+# @api_view(["GET"])
+# def api_home(request, *args, **kwargs):
+#     """
+#     DRF API View
+#     """
+#     model_data = Product.objects.all().order_by("?").first() # This will give a queryset
+#     print("model_data => ",model_data)
+#     data = {}
+#     if model_data:
+#         data = model_to_dict(model_data, fields=["id", "title", "price", "sale_price"]) # This will convert the queryset to a dictionary 
+#         print("Data =>",data)
+#     return Response(data) # This will return clean JSON type response
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    model_data = Product.objects.all().order_by("?").first() # This will give a queryset
-    print("model_data => ",model_data)
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "title", "price"]) # This will convert the queryset to a dictionary 
-        print("Data =>",data)
-    return Response(data) # This will return clean JSON type response
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
