6ed080fbbf7292cb3b941ad28eadf95511c4ed7a
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-25 00:57:21 +0530

    Checking out Viewsets and Routers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -9,6 +9,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            "pk",
             "title",
             "content",
             "price",
