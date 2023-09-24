30c9b183fe72af561874ceb151100be445f080d9
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-24 20:09:32 +0530

    Checking the working of token authentication

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,27 @@
+# import requests
+
+# endpoint = "http://localhost:8000/api/products/" 
+
+# get_response = requests.get(endpoint) 
+# print(get_response.json())
+
 import requests
+from getpass import getpass #! for secure password
 
-endpoint = "http://localhost:8000/api/products/" 
+auth_endpoint = "http://localhost:8000/api/auth/" 
+username = input("What is your username?\n")
+password = getpass("What is your password?\n")
 
-get_response = requests.get(endpoint) 
-print(get_response.json())
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password}) 
+print(auth_response)
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/" 
+
+    get_response = requests.get(endpoint, headers=headers) 
+    print(get_response.json())
