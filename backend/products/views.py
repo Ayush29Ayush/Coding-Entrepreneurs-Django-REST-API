0a1a4797b3b171f7330f20e83a7c8e4cff3d2fa6
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-24 19:20:35 +0530

    Checking out custom permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 #! CreateAPIView => Used for create-only endpoints. Provides a post method handler.
@@ -13,7 +14,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
