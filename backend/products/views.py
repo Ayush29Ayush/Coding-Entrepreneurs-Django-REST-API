fbb3c06d3ed7a7eb4fad9638ec461d4ad21eeda4
Ayush Senapati <ayush.senapati@cozentus.com>
2023-09-24 18:55:57 +0530

    Checking out User & Group Permissions with DjangoModelPermissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -13,7 +13,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
